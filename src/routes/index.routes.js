b619a6e66954bef47ea1929cf1e41a985cc1ed69
Eduardo Velazques <eduwavee@gmail.com>
2025-12-10 22:48:46 -0300

    Pedidos funcionando

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import productosRoutes from "./productos.routes.js";
+import pedidosRoutes from "./pedidos.routes.js";
+
 
 const router = Router();
 
 router.use("/productos", productosRoutes);
+router.use("/pedidos", pedidosRoutes);
+
 
 export default router;
