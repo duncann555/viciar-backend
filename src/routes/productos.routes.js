34087226a51e54622ab77cb96feeebcd8873c3d9
Luis Robles <luisgeremiasrobles@gmail.com>
2025-12-11 14:43:23 -0300

    Agregue validaciones para los productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -9,18 +9,29 @@ import {
 } from "../controllers/productos.controllers.js";
 import upload from "../helpers/upload.js";
 import errorMulter from "../middlewares/ErrorMulter.js";
+import validacionProducto from "../middlewares/validacionProducto.js";
 
 const router = Router();
 
 router
   .route("/")
-  .post(upload.single("imagenUrl"), errorMulter, crearProducto)
+  .post(
+    upload.single("imagenUrl"),
+    errorMulter,
+    validacionProducto,
+    crearProducto
+  )
   .get(listarProductos);
 router.route("/buscar").get(filtrarProductoNombre);
 router
   .route("/:id")
   .delete(eliminarProducto)
   .get(obtenerProductoID)
-  .put(upload.single("imagenUrl"), errorMulter, editarProducto);
+  .put(
+    upload.single("imagenUrl"),
+    errorMulter,
+    validacionProducto,
+    editarProducto
+  );
 
 export default router;
