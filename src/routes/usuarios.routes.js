9ca22e1f95383a8335432c27641a3e796ddddf3e
Luis Robles <luisgeremiasrobles@gmail.com>
2025-12-12 11:49:36 -0300

    Agregue verificacion de token y rol en la ruta de usuarios#

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -6,10 +6,15 @@ import {
 } from "../controllers/usuarios.controllers.js";
 import validacionUsuarios from "../middlewares/validacionUsuarios.js";
 import validacionLogin from "../middlewares/validacionLogin.js";
+import verificarJWT from "../middlewares/verificarToken.js";
+import { EsAdmin } from "../middlewares/verificarRoles.js";
 
 const router = Router();
 
-router.route("/").post(validacionUsuarios, crearUsuario).get(listarUsuarios);
+router
+  .route("/")
+  .post(validacionUsuarios, crearUsuario)
+  .get(verificarJWT, EsAdmin, listarUsuarios);
 router.route("/login").post(validacionLogin, login);
 
 export default router;
