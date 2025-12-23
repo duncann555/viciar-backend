0c4e39375a55bfa5509abf0e3e84e87113eb3eac
Eduardo Velazques <eduwavee@gmail.com>
2025-12-23 00:14:04 -0300

    Se corrigieron las validaciones del login

--- src/middlewares/validacionLogin.js
+++ src/middlewares/validacionLogin.js
@@ -1,19 +1,28 @@
 import { body } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
-import Usuario from "../models/usuario.js";
 
 const validacionLogin = [
   body("email")
     .notEmpty()
-    .withMessage("El email es un dato obligatorio")
+    .withMessage("El email es obligatorio")
     .isEmail()
-    .withMessage("El email ingresado no es valido")
+    .withMessage("Email inválido")
     .normalizeEmail(),
+
   body("password")
     .notEmpty()
-    .withMessage("La contraseña es un dato obligatorio")
+    .withMessage("La contraseña es obligatoria")
     .isLength({ min: 8 })
-    .withMessage("La contraseña debe contener minimo 8 caracteres"),
+    .withMessage("La contraseña debe tener al menos 8 caracteres")
+    .matches(/[A-Z]/)
+    .withMessage("Debe contener al menos una mayúscula")
+    .matches(/[a-z]/)
+    .withMessage("Debe contener al menos una minúscula")
+    .matches(/\d/)
+    .withMessage("Debe contener al menos un número")
+    .matches(/[@$!%*?&]/)
+    .withMessage("Debe contener al menos un carácter especial"),
+
   (req, res, next) => resultadoValidacion(req, res, next),
 ];
 
